5739bea89ccf3e77938a78d9259cd589c294f12b
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-12-02 09:49:24 +0200

    Day 2 - Part 2

--- day02.fsx
+++ day02.fsx
@@ -11,6 +11,7 @@ type Instruction = {
 type Position = {
     X : int
     Depth : int
+    Aim : int // Part 2
 }
 
 let parseInstructions (xs : seq<string>) =
@@ -41,10 +42,29 @@ let processInstructions instructions =
         | Up -> { current with Depth = current.Depth - instruction.Steps }
 
     instructions
-    |> Seq.fold processor { X = 0; Depth = 0 }
+    |> Seq.fold processor { X = 0; Depth = 0; Aim = 0 }
 
+// Part 1
 let getPosition filename =
     filename
     |> getInstructions
     |> processInstructions
+    |> (fun i -> i.X * i.Depth)
+
+// Part 2
+let processInstructionsWithAim instructions =
+    let processor current instruction =
+        match instruction.Operation with
+        | Forward -> { current with X = current.X + instruction.Steps;
+                                    Depth = current.Depth + (instruction.Steps * current.Aim) }
+        | Down -> { current with Aim = current.Aim + instruction.Steps }
+        | Up -> { current with Aim = current.Aim - instruction.Steps }
+
+    instructions
+    |> Seq.fold processor { X = 0; Depth = 0; Aim = 0 }
+
+let getPositionWithAim filename =
+    filename
+    |> getInstructions
+    |> processInstructionsWithAim
     |> (fun i -> i.X * i.Depth)
