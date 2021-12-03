997c5f5ce4fe20528eff08121e54327a814dbcc5
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-12-04 00:46:35 +0200

    Day3 - Part 2

--- day03.fsx
+++ day03.fsx
@@ -34,5 +34,38 @@ let getPowerConsumption filename =
     let lsb = msb |> invert
     let gamma = msb |> toDecimal
     let epsilon = lsb |> toDecimal
-
     (gamma * epsilon)
+
+// Part 2
+let getByBitCriteria (includeZerosFunc : int -> int -> bool) (report : int[,]) =
+    let rec loop i (report : int[,]) =
+        let len = report |> Array2D.length1
+
+        if len = 1 then
+            report.[0,*]
+        else
+            let arr = report.[*,i]
+            let ones = arr |> Array.filter (fun n -> n = 1) |> Array.length
+            let zeros = len - ones
+            let includeZeros = includeZerosFunc zeros ones
+            let includeOnes = not(includeZeros)
+
+            [|
+                for j in 0..(len - 1) do
+                    if (report.[j,i] = 0 && includeZeros) || (report.[j,i] = 1 && includeOnes) then
+                        yield report.[j,*]
+            |] |> array2D |> loop (i + 1)
+
+    loop 0 report
+
+let getOxygenGeneratorRating (report : int[,]) =
+    report |> getByBitCriteria (fun zeros ones -> zeros > ones)
+
+let getCO2ScrubberRating (report : int[,]) =
+    report |> getByBitCriteria (fun zeros ones -> not(zeros > ones))
+
+let getLifeSupportRating filename =
+    let report = getDiagnosticReport filename
+    let oxygen = getOxygenGeneratorRating report |> toDecimal
+    let scrubber = getCO2ScrubberRating report |> toDecimal
+    (oxygen * scrubber)
