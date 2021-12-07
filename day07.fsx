70fb12f8ab6009e4777f053e43ba008c8c1cec48
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-12-07 09:23:24 +0200

    Day 7 - Part 2

--- day07.fsx
+++ day07.fsx
@@ -22,3 +22,22 @@ let getCheapestPosition filename =
     |> getAllPositions
     |> Seq.map (fun i -> (i, calculateFuelForPosition positions i))
     |> Seq.minBy (fun (_, fuel) -> fuel)
+
+// Part 2
+let calculateFuelForPositionV2 (positions : Map<int, int>) i =
+    let fuel (k, v) =
+        [0..abs(k - i)]
+        |> List.sum
+        |> (*) v
+
+    positions
+    |> Map.toSeq
+    |> Seq.map fuel
+    |> Seq.sum
+
+let getCheapestPositionV2 filename =
+    let positions = getPositions filename
+    positions
+    |> getAllPositions
+    |> Seq.map (fun i -> (i, calculateFuelForPositionV2 positions i))
+    |> Seq.minBy (fun (_, fuel) -> fuel)
