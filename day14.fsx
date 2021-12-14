2bb9b469de9a95df8bf3de35ce875411775c7287
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-12-14 11:16:59 +0200

    Day 14 - Part 2

--- day14.fsx
+++ day14.fsx
@@ -4,7 +4,16 @@ let getData filename =
 
     let template =
         lines.[0]
-        |> Seq.toList
+        |> Seq.pairwise
+        |> Seq.countBy id
+        |> Seq.map (fun (k,v) -> (k, int64 v))
+        |> Map.ofSeq
+
+    let elementCount =
+        lines.[0]
+        |> Seq.countBy id
+        |> Seq.map (fun (k,v) -> (k, int64 v))
+        |> Map.ofSeq
 
     let insertionRules =
         lines.[2..]
@@ -14,29 +23,66 @@ let getData filename =
             | _ -> None)
         |> Map.ofSeq
 
-    (template, insertionRules)
+    (template, elementCount, insertionRules)
 
-let processData template (insertionRules : Map<(char * char), char>) =
-    let template' =
-        template
-        |> List.pairwise
-        |> List.map (fun (a, b) ->
+let processData 
+    (pairs : Map<(char * char), int64>)
+    (elementCount : Map<char, int64>)
+    (insertionRules : Map<(char * char), char>) =
+
+    let tmp = pairs |> Map.toSeq |> dict |> System.Collections.Generic.Dictionary
+    let tmpCnt = elementCount |> Map.toSeq |> dict |> System.Collections.Generic.Dictionary
+
+    pairs
+    |> Map.toSeq
+    |> Seq.iter (fun ((a,b), times) ->
             let c = insertionRules.[(a,b)]
-            [a; c])
-        |> List.concat
 
-    template' @ [template.[^0]]
+            let ac = match tmp.TryGetValue((a,c)) with | (true, v) -> v | _ -> 0
+            tmp.[(a,c)] <- (ac + times)
 
-let part1 filename =
-    let (template, insertionRules) = getData filename
-    let folder template _ = processData template insertionRules
+            let cb = match tmp.TryGetValue((c,b)) with | (true, v) -> v | _ -> 0
+            tmp.[(c,b)] <- (cb + times)
+
+            let ab = match tmp.TryGetValue((a,b)) with | (true, v) -> v | _ -> 0
+            tmp.[(a,b)] <- (ab - times)
+
+            let cv = match tmpCnt.TryGetValue(c) with | (true, v) -> v | _ -> 0
+            tmpCnt.[c] <- cv + times
+        )
+
+    let pairs =
+        tmp
+        |> Seq.map (|KeyValue|)
+        |> Seq.filter (fun (_,v) -> v > 0L)
+        |> Map.ofSeq
+
+    let elementCount =
+        tmpCnt
+        |> Seq.map (|KeyValue|)
+        |> Map.ofSeq
+
+    (pairs, elementCount)
+
+let execute filename times =
+    let (template, elementCount, insertionRules) = getData filename
+    let folder (template, elementCount) _ =
+        processData template elementCount insertionRules
 
     let cnt =
-         [1..10]
-         |> List.fold folder template
-         |> List.countBy id
-         |> List.map snd
+        [1..times]
+        |> List.fold folder (template, elementCount)
+        |> (fun (_, cnt) ->
+            cnt
+            |> Map.toSeq
+            |> Seq.map snd)
 
-    let min = cnt |> List.min
-    let max = cnt |> List.max
+    let min = cnt |> Seq.min
+    let max = cnt |> Seq.max
     max - min
+
+let part1 filename =
+    execute filename 10
+
+let part2 filename =
+    execute filename 40
