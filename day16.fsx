3661907a1f5bb7790aef5690d6628f5cc0cadcf1
Martin Björkström <martin.bjorkstrom@gmail.com>
2021-12-16 21:32:26 +0200

    Day 16 - Part 1
    - Code needs cleanup, it's a mess

--- day16.fsx
+++ day16.fsx
@@ -7,7 +7,7 @@ type PackageHeader = {
 }
 
 type Package =
-    | Literal of (PackageHeader * int)
+    | Literal of (PackageHeader * int64)
     | Operator of (PackageHeader * Package list)
 
 let hexToBin (str : seq<char>) =
@@ -19,8 +19,11 @@ let hexToBin (str : seq<char>) =
                 |> (fun b -> Convert.ToString(b, 2).PadLeft(4, '0'))
     }
 
-let binToDec (str : string) =
+let binToInt32 (str : string) =
     Convert.ToInt32(str, 2)
+
+let binToInt64 (str : string) =
+    Convert.ToInt64(str, 2)
 
 let rec parse (input : seq<char>) =
     let advance (e : IEnumerator<char>) =
@@ -39,44 +42,54 @@ let rec parse (input : seq<char>) =
     let parseInt cnt e =
         let arr = e |> take cnt |> Seq.toArray
         if arr.Length <> cnt then None
-        else arr |> String |> binToDec |> Some
+        else arr |> String |> binToInt32 |> Some
 
     let parsePackageHeader (e : IEnumerator<char>) =
         if advance e then
             match e |> parseInt 3 with
-            | None | Some 0 -> None
+            | None -> None
             | Some v ->
                 if advance e then
                     match e |> parseInt 3 with
-                    | None | Some 0 -> None
+                    | None -> None
                     | Some t -> Some { Version = v; Type = t }
                 else None
         else None
 
     let parseLiteral (e : IEnumerator<char>) =
         [|
-            let mutable groups = 0
             let mutable continue' = true
             while continue' do
-                groups <- groups + 1
                 e.MoveNext() |> ignore
                 continue' <- e.Current = '1'
                 e.MoveNext() |> ignore
                 let v = (e |> take 4)
                 yield! v
-        |] |> String |> binToDec
+        |] |> String |> binToInt64
 
-    let parseOperator (e : IEnumerator<char>) =
+    let rec parseOperator (e : IEnumerator<char>) =
         e.MoveNext() |> ignore
         let lenBits = if e.Current = '0' then 15 else 11
         e.MoveNext() |> ignore
-        let len = e |> parseInt lenBits |> Option.get
-        e.MoveNext() |> ignore
-
-        e
-        |> take len
-        |> parse
-        |> Seq.toList
+        match e |> parseInt lenBits with
+        | None -> []
+        | Some len ->
+            if len = 15 then // length
+                e.MoveNext() |> ignore
+                e |> take len |> parse |> Seq.toList
+            else // subpackets
+                // e |> advance |> ignore
+                [
+                    for _ in 1..len do
+                        let opt = e |> parsePackageHeader
+                        if opt |> Option.isSome then
+                            let header = opt |> Option.get
+                            let package =
+                                match header.Type with
+                                | 4 -> Literal(header, e |> parseLiteral)
+                                | _ -> Operator(header, e |> parseOperator)
+                            yield package
+                ]
 
     seq {
         let e = input.GetEnumerator()
@@ -100,18 +113,22 @@ let parseHex (input : string) =
     |> hexToBin
     |> parse
 
-"00111000000000000110111101000101001010010001001000000000" |> parse
+let rec sumVersion (packages : seq<Package>) =
+    packages
+    |> Seq.fold (fun sum p ->
+        match p with
+        | Literal (header, _) -> sum + header.Version
+        | Operator (header, subs) ->
+            subs |> sumVersion |> (+) (sum + header.Version)
+        ) 0
 
-"8A004A801A8002F478" |> parseHex
+let part1 filename =
+    filename
+    |> System.IO.File.ReadAllText
+    |> parseHex
+    |> sumVersion
 
-"110100101111111000101000" |> parse
-
-"00111000000000000110111101000101001010010001001000000000" |> parse
-"11010001010" |> parse
-"0101001000100100" |> parse
-"110100010100101001000100100" |> parse
-"110100101111111000101110100101111111000101" |> parse
-
-"110100101111111000101000" |> parse
-
-"000" |> parse
+"8A004A801A8002F478" |> parseHex |> sumVersion
+"620080001611562C8802118E34" |> hexToBin |> parse |> sumVersion
+"C0015000016115A2E0802F182340" |> parseHex |> sumVersion
+"A0016C880162017C3686B18A3D4780" |> parseHex |> sumVersion
